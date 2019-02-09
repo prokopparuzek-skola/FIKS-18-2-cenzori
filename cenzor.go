9fb8b4225a954e90f2a1249e954889becc163ab5
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2019-02-09 17:33:09 +0100

    Initial

---
+++ cenzor.go
@@ -0,0 +1,48 @@
+package main
+
+import "fmt"
+
+type input_t struct {
+	t uint64
+	b uint64
+	e uint64
+	A uint64
+}
+
+type generator_t struct {
+	a uint64
+	b uint64
+	x uint64
+}
+
+func (gen *generator_t) nextInt() uint64 {
+	gen.x = (gen.x*gen.a + gen.b) % uint64(10000000007)
+	return gen.x
+}
+
+func (gen *generator_t) genInput(in *input_t, N uint64) {
+	in.t = gen.nextInt() % 3
+	in.b = gen.nextInt() % N
+	in.e = gen.nextInt() % N
+	if in.b > in.e {
+		swap := in.e
+		in.e = in.b
+		in.b = swap
+	}
+	in.A = gen.nextInt() % N
+}
+
+func oneTask(gen *generator_t) {
+
+}
+
+func main() {
+	var T uint8
+	fmt.Scanf("%d", &T)
+	for i := uint8(0); i < T; i++ {
+		var generator generator_t
+		var N, t uint64
+
+		fmt.Scanf("%d %d %d %d %d", &t, &N, &generator.a, &generator.b, &generator.x)
+	}
+}
